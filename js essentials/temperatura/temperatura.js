cdecc759908d8acf35550e5088051076cd26b3c3
Fellipe Nunes <68883903+Fellipe-Nunes@users.noreply.github.com>
2020-09-22 15:57:44 -0300

    Add files via upload
    códigos de testes js essentials

---
+++ js essentials/temperatura/temperatura.js
@@ -0,0 +1,12 @@
+function celsius (temperatura){
+    let celsius = 5*((temperatura-32)/9)
+    console.log(celsius);
+}
+
+const fahrenheit = (temperatura) =>{
+    let celsius = 5*((temperatura-32)/9)
+    console.log(celsius);
+}
+
+celsius(20);
+fahrenheit(90);
